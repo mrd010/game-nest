46f9b2e197b672e35eed6049d96dd6119d95a95e
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-28 23:18:56 +0330

    Add google material symbols

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'material-symbols/rounded.css';
 import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
