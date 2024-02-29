386551f8d12a53a1e14b86eddac3f3e42d0b6a87
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 14:00:58 +0330

    Rename helpers folder to services

--- src/helpers/userInfoHelpers.js
+++
@@ -1,7 +0,0 @@
-const getUserLanguage = () => {
-  const language = navigator.language;
-  //   get only first part of language . ex: en-US => en
-  return language.split('-')[0];
-};
-
-export default getUserLanguage;
