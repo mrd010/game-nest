56eb064b52033e515210dbb48ecd00daade767f8
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 15:18:42 +0330

    Add first part of load data first part : get recommended games

--- src/loaders/homeLoader.js
+++ src/loaders/homeLoader.js
@@ -1,10 +1,14 @@
-import getUserLanguage from '../helpers/userInfoHelpers';
+import { getRecommendedGames } from '../services/dataFetchers';
+import getUserLanguage from '../services/userLocaleServices';
 
 // recommended games should be above this threshold
 const recommendedGamesMinScore = 80;
+// get user lang
 const userLanguage = getUserLanguage();
-const recommendedGamesApi = `https://www.cheapshark.com/api/1.0/deals?AAA=1&sortBy=Release&metacritic=${recommendedGamesMinScore}`;
-const featuredCategoriesApi = 'https://store.steampowered.com/api/featuredcategories';
-const trailersApi = `https://store.steampowered.com/api/trailerslideshow/?l=${userLanguage}`;
 
-export const homeLoader = () => {};
+export const homeLoader = () => {
+  const recommendedGamesPromise = getRecommendedGames(recommendedGamesMinScore);
+
+  Promise.all([recommendedGamesPromise]).then((data) => console.log(data));
+  return null;
+};
