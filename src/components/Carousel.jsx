4444db0ed367eb4542298d4b3ad85e2236bddee5
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 00:47:15 +0330

    Change buttons to seprate component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
+import ActionButton from './ActionButton';
 const Carousel = ({ title, children }) => {
   return (
-    <div className="grid grid-cols-2 grid-row-2 gap-2">
-      <h2 className="text-2xl font-bold">{title}</h2>
-      <div>
-        <button>
+    <div className="grid grid-cols-[1fr_auto] grid-row-2 gap-5 carousel">
+      <h2 className="text-2xl font-bold border-b-[1px] pb-2 border-gray-900/50">{title}</h2>
+      <div className="flex flex-row gap-2 justify-self-end self-end">
+        <ActionButton>
           <span className="material-symbols-rounded">arrow_back</span>
-        </button>
-        <button>
+        </ActionButton>
+        <ActionButton>
           <span className="material-symbols-rounded">arrow_forward</span>
-        </button>
+        </ActionButton>
       </div>
       {children && (
-        <div className="grid grid-flow-col auto-cols-[300px] gap-5 overflow-x-auto scroll-smooth col-span-2">
+        <div className="grid grid-flow-col auto-cols-[300px] gap-4  scroll-smooth col-span-2 overflow-x-auto pb-2">
           {children}
         </div>
       )}
