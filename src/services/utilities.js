36af89e4a60af57a243e2faa7aa285eff3ccd248
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 20:11:10 +0330

    Create image fetchers

---
+++ src/services/utilities.js
@@ -0,0 +1,9 @@
+export const steamHeaderImage = (steamId) => {
+  return `https://cdn.akamai.steamstatic.com/steam/apps/${steamId}/header.jpg`;
+};
+// export const steamLargeCapsuleImage = (steamId) => {
+//   return `https://cdn.akamai.steamstatic.com/steam/apps/${steamId}/header.jpg`;
+// };
+export const steamThumbnail = (steamId) => {
+  return `https://cdn.akamai.steamstatic.com/steam/apps/${steamId}/capsule_184x69.jpg`;
+};
