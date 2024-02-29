76868c12abd6cca68a61d68b4403d8f6ec85706f
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 00:02:20 +0330

    Add layout for component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 const Carousel = ({ title, children }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <button>
-        <span className="material-symbols-rounded">arrow_back</span>
-      </button>
-      <button>
-        <span className="material-symbols-rounded">arrow_forward</span>
-      </button>
-      {children && <div>{children}</div>}
+    <div className="grid grid-cols-2 grid-row-2 gap-2">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <div>
+        <button>
+          <span className="material-symbols-rounded">arrow_back</span>
+        </button>
+        <button>
+          <span className="material-symbols-rounded">arrow_forward</span>
+        </button>
+      </div>
+      {children && (
+        <div className="grid grid-flow-col auto-cols-[300px] gap-5 overflow-x-auto scroll-smooth col-span-2">
+          {children}
+        </div>
+      )}
     </div>
   );
 };
