d6773ec6a987c519b12126527cc790e9a04c3a26
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 20:22:39 +0330

    Add carousel base structure

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,20 @@
 import PropTypes from 'prop-types';
-const Carousel = ({ children }) => {
-  return <div></div>;
+const Carousel = ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button>
+        <span className="material-symbols-outlined">west</span>
+      </button>
+      <button>
+        <span className="material-symbols-outlined">east</span>
+      </button>
+      <div>{children}</div>
+    </div>
+  );
 };
-Carousel.propTypes = {};
+Carousel.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.oneOfType(PropTypes.node, PropTypes.arrayOf(PropTypes.node)),
+};
 export default Carousel;
