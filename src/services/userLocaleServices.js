b3c3f3b8981d4420e54f061e2a31b4d12dbf7467
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 17:51:56 +0330

    Make returned lang code lowercase

--- src/services/userLocaleServices.js
+++ src/services/userLocaleServices.js
@@ -4,7 +4,7 @@ const getUserLanguage = () => {
   // get language alpha 2 from system
   const languageCode = navigator.language.split('-')[0].toLowerCase();
   // get language full name from data
-  langCodes.find((lang) => lang.alpha2 === languageCode).English;
+  return langCodes.find((lang) => lang.alpha2 === languageCode).English.toLowerCase();
 };
 
 export default getUserLanguage;
