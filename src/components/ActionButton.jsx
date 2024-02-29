c252f903fce678d48c414b4246d7144f7d2ddcb0
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 02:00:18 +0330

    Add disable button style

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
-const ActionButton = ({ children }) => {
+const ActionButton = ({ isDisabled, onClick, children }) => {
   return (
-    <button className="rounded-full bg-gray-200 hover:bg-gray-800 hover:text-gray-50 hover:opacity-100 size-8 place-items-center grid transition-colors">
+    <button
+      className="rounded-full bg-gray-200 [&:hover:not(:disabled)]:bg-gray-800 [&:hover:not(:disabled)]:text-gray-50 hover:opacity-100 size-8 place-items-center grid transition-colors disabled:opacity-25 transition-opacity"
+      onClick={onClick}
+      disabled={isDisabled}
+    >
       {children}
     </button>
   );
 };
 ActionButton.propTypes = {
+  isDisabled: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
 export default ActionButton;
