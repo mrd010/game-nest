2d90f41f6bb3ceaa0495b963ee7d93b90c1f901d
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 00:46:46 +0330

    Add recommended games carousel

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,23 @@
+import { useLoaderData } from 'react-router-dom';
+import Carousel from '../components/Carousel';
+import RecommendedCard from '../components/RecommendedCard';
+
 const Home = () => {
-  return <div>Home</div>;
+  const { recommendedGames, featuredCategories, trailers } = useLoaderData();
+  console.log(recommendedGames);
+
+  return (
+    <div>
+      <section className="my-5">
+        {recommendedGames.length && (
+          <Carousel title="Recommended Games">
+            {recommendedGames.map((game) => (
+              <RecommendedCard key={game.steamAppID} {...game}></RecommendedCard>
+            ))}
+          </Carousel>
+        )}
+      </section>
+    </div>
+  );
 };
 export default Home;
