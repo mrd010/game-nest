f5f853bb8fb1b8f06c866d265ed1793585b181b0
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-29 14:16:44 +0330

    Add router to both Home routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,13 +8,14 @@ import Layout from './Layout';
 import Home from './routes/Home';
 import Games from './routes/Games';
 import Deals from './routes/Deals';
+import { homeLoader } from './loaders/homeLoader';
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="home" element={<Home />}></Route>
+        <Route index element={<Home />} loader={homeLoader}></Route>
+        <Route path="home" element={<Home />} loader={homeLoader}></Route>
         <Route path="games" element={<Games />}></Route>
         <Route path="deals" element={<Deals />}></Route>
       </Route>
