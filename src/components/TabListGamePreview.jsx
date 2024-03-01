5416d833ced59d7030fe9571979eb62b213c2c9c
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 00:08:01 +0330

    Change name of file

---
+++ src/components/TabListGamePreview.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+const TabListGamePreview = ({ id }) => {
+  return <div>TabListGamePreview</div>;
+};
+TabListGamePreview.propTypes = {
+  id: PropTypes.number,
+};
+export default TabListGamePreview;
--- src/components/TabListPreviewPanel.jsx
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-const TabListPreviewPanel = () => {
-  return <div>TabListPreviewPanel</div>;
-};
-TabListPreviewPanel.propTypes = {};
-export default TabListPreviewPanel;
