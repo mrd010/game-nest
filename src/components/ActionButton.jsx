773daed11ecbe856827ec892f75fc843f894c82e
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 11:54:25 +0330

    Remove op tr

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 const ActionButton = ({ isDisabled, onClick, children }) => {
   return (
     <button
-      className="rounded-full bg-gray-200 [&:hover:not(:disabled)]:bg-gray-800 [&:hover:not(:disabled)]:text-gray-50 hover:opacity-100 size-8 place-items-center grid transition-colors disabled:opacity-25 transition-opacity"
+      className="rounded-full bg-gray-200 [&:hover:not(:disabled)]:bg-gray-800 [&:hover:not(:disabled)]:text-gray-50 hover:opacity-100 size-8 place-items-center grid transition-colors disabled:opacity-25"
       onClick={onClick}
       disabled={isDisabled}
     >
