2a40d6e4bd4316dd0f54aa19bb3de92ae2b4396e
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 19:32:55 +0330

    Add scroller bottom of tabs

--- src/components/TabbedCategories.jsx
+++ src/components/TabbedCategories.jsx
@@ -5,13 +5,32 @@ import GamesListRow from './GamesListRow';
 const TabbedCategories = ({ categoriesData }) => {
   const [selectedTab, setSelectedTab] = useState('new_releases');
   const [pageNumber, setPageNumber] = useState(0);
+
+  const tabIndex = selectedTab === 'new_releases' ? 0 : selectedTab === 'top_sellers' ? 1 : 2;
   console.log(categoriesData);
   return (
     <div>
-      <div>
-        <TabButton onSelect={() => setSelectedTab('new_releases')}>New Releases</TabButton>
-        <TabButton onSelect={() => setSelectedTab('top_sellers')}>Top Sellers</TabButton>
-        <TabButton onSelect={() => setSelectedTab('coming_soon')}>Coming Soon</TabButton>
+      <div
+        className={`flex relative after:content-[' '] after:h-1 after:rounded-sm after:bg-yellow-500 after:w-32 after:absolute after:bottom-0 after:transition-transform ${tabIndex === 1 ? 'after:translate-x-32' : tabIndex === 2 ? 'after:translate-x-64' : ''}`}
+      >
+        <TabButton
+          onSelect={() => setSelectedTab('new_releases')}
+          isSelected={selectedTab === 'new_releases'}
+        >
+          New Releases
+        </TabButton>
+        <TabButton
+          onSelect={() => setSelectedTab('top_sellers')}
+          isSelected={selectedTab === 'top_sellers'}
+        >
+          Top Sellers
+        </TabButton>
+        <TabButton
+          onSelect={() => setSelectedTab('coming_soon')}
+          isSelected={selectedTab === 'coming_soon'}
+        >
+          Coming Soon
+        </TabButton>
       </div>
       <div>
         {categoriesData[selectedTab]
