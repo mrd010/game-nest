13550b3b3ddf232224e9e36259fe0ab60515aedc
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-01 18:59:05 +0330

    Fix size of icons

--- src/components/OSIcon.jsx
+++ src/components/OSIcon.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 const OSIcon = ({ src }) => {
   return (
     <div>
-      <img src={src} alt="OS Icon" />
+      <img src={src} alt="OS Icon" width={20} height={20} />
     </div>
   );
 };
