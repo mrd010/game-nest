c04e880c75f762b280158784057a4d7252e5d0d4
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 15:10:23 +0330

    Remove unused vars

--- src/components/GameScore.jsx
+++ src/components/GameScore.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { number } from 'prop-types';
+import PropTypes from 'prop-types';
 import { getMetascoreColor } from '../services/utilities';
 const GameScore = ({ children }) => {
   return (
