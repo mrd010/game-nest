5b8b015a614cde95b29635d7e7b7cf5ab1e76327
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 23:51:21 +0330

    Rename func to array

--- src/services/parsers.js
+++ src/services/parsers.js
@@ -1,6 +1,6 @@
 // gets a string of game system requirements in html format and converts it to object
 // format:<strong>Minimum:</strong><br><ul class=\"bb_ul\"><li><strong>OS:</strong> Windows® 7 (32/64-bit)/Vista/XP<br>
-export const htmlToSysReqObject = (sysReqHtml) => {
+export const htmlToSysReqArray = (sysReqHtml) => {
   // extract text from html code. output => os:   windows   graphics:  some card   ram:  how much
   const sysReqText = sysReqHtml.replace(/<[^>]*(>|$)| |‌|»|«|>/g, ' ');
   //   convert text to an array of sys req part string. output => [os:  windows, graphics:  some card]
