0f0b83792f34eefeaaf58e58f03c6422e2476067
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 12:30:11 +0330

    Create image loader placeholder

--- src/components/RecommendedCard.jsx
+++ src/components/RecommendedCard.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { steamHeaderImage } from '../services/utilities';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const RecommendedCard = ({
   steamAppID,
@@ -12,12 +13,20 @@ const RecommendedCard = ({
   releaseDate,
 }) => {
   // game card for each game in recommended games section . links to game page
-
+  const [imageLoaded, setImageLoaded] = useState(false);
   // TODO
   return (
     <Link className="gap-2 p-3 drop-shadow-sm shadow-md bg-gradient-to-b from-amber-300 from-25% to-25% to-gray-100 rounded-md w-[290px]">
-      <div className="flex flex-col flex-nowrap gap-2">
-        <img src={steamHeaderImage(steamAppID)} alt={`${title} image`} className="rounded-md" />
+      <div className="grid grid-rows-[124px_auto] gap-3 ">
+        <div className="size-full relative rounded-md bg-gray-100">
+          {!imageLoaded && <div className="image-loader rounded-md absolute top-0 left-0"></div>}
+          <img
+            src={steamHeaderImage(steamAppID)}
+            alt={`${title} image`}
+            className="h-[124px] w-full rounded-md"
+            onLoad={() => setImageLoaded(true)}
+          />
+        </div>
         <h3 className="text-2xl font-bold ">{title}</h3>
         <span>Released on {Date(releaseDate)}</span>
       </div>
