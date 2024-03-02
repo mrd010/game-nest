6499debb03cfe421739a54c56e39e2bcd17e8f2a
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 19:33:06 +0330

    Create a base layout of preview panel with few options

--- src/components/TabListGamePreview.jsx
+++ src/components/TabListGamePreview.jsx
@@ -1,6 +1,20 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 const TabListGamePreview = ({ id }) => {
-  return <div>TabListGamePreview</div>;
+  const [gameData, setGameData] = useState(null);
+  return (
+    <>
+      {gameData ? (
+        <div>
+          <img src={gameData.header_image} alt={gameData.name} />
+          <h3>{gameData.name}</h3>
+          <p>{gameData.short_description}</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
 };
 TabListGamePreview.propTypes = {
   id: PropTypes.number,
