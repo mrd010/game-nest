f3d6bc45eebce497fed386462d3f3e90afa630bd
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 20:18:54 +0330

    Add comments

--- src/components/TabListGamePreview.jsx
+++ src/components/TabListGamePreview.jsx
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types';
 import { useData } from '../hooks/useData';
 import { steamHeaderImage } from '../services/utilities';
 const TabListGamePreview = ({ id }) => {
+  // if a game selected fetch its data else data is empty
   const { data } = useData(id ? `/api/appdetails?appids=${id}` : null);
+  // if data fetched and data is for this id and it fetched with succuss set game data
   const gameData = data && data[id] && data[id].success ? data[id].data : null;
-  console.log(data);
 
   return (
     <>
       {gameData ? (
         <div>
-          <img width={460} height={215} src={steamHeaderImage(id)} alt={gameData.name} />
+          <img src={steamHeaderImage(id)} alt={gameData.name} />
           <h3>{gameData.name}</h3>
           <p>{gameData.short_description}</p>
         </div>
