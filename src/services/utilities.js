bbf0e7bd671465dedf767dacc163314c85a34ea8
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 01:02:30 +0330

    Remove check for gamedata

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -34,31 +34,35 @@ export const getMetascoreColor = (score) => {
 
 export const extractGameSysReq = (gameData) => {
   let systemRequirements = {};
-  if (gameData) {
-    if (gameData.pc_requirements && gameData.pc_requirements.minimum) {
-      const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.minimum);
-      if (sysReqArray.length) {
-        systemRequirements.pcMinimum = sysReqArray;
-      }
-    }
-    if (gameData.pc_requirements && gameData.pc_requirements.recommended) {
-      const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.recommended);
-      if (sysReqArray.length) {
-        systemRequirements.pcRecommended = sysReqArray;
-      }
-    }
-    if (gameData.mac_requirements && gameData.mac_requirements.minimum) {
-      const sysReqArray = htmlToSysReqArray(gameData.mac_requirements.minimum);
-      if (sysReqArray.length) {
-        systemRequirements.macMinimum = sysReqArray;
-      }
-    }
-    if (gameData.linux_requirements && gameData.linux_requirements.minimum) {
-      const sysReqArray = htmlToSysReqArray(gameData.linux_requirements.minimum);
-      if (sysReqArray.length) {
-        systemRequirements.linuxMinimum = sysReqArray;
-      }
+
+  if (gameData.pc_requirements && gameData.pc_requirements.minimum) {
+    const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.minimum);
+    if (sysReqArray.length) {
+      systemRequirements.pcMinimum = sysReqArray;
     }
   }
+  if (gameData.pc_requirements && gameData.pc_requirements.recommended) {
+    const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.recommended);
+    if (sysReqArray.length) {
+      systemRequirements.pcRecommended = sysReqArray;
+    }
+  }
+  if (gameData.mac_requirements && gameData.mac_requirements.minimum) {
+    const sysReqArray = htmlToSysReqArray(gameData.mac_requirements.minimum);
+    if (sysReqArray.length) {
+      systemRequirements.macMinimum = sysReqArray;
+    }
+  }
+  if (gameData.linux_requirements && gameData.linux_requirements.minimum) {
+    const sysReqArray = htmlToSysReqArray(gameData.linux_requirements.minimum);
+    if (sysReqArray.length) {
+      systemRequirements.linuxMinimum = sysReqArray;
+    }
+  }
+
   return systemRequirements;
+};
+
+export const getPriorityPlatform = (platforms) => {
+  return Object.entries(platforms).find((platform) => platform[1] === true)[0];
 };
