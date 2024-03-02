c50afb2ee934923606a262c83d911b44d23649d3
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-02 20:27:30 +0330

    Create empty preview panel layout

--- src/components/TabListGamePreview.jsx
+++ src/components/TabListGamePreview.jsx
@@ -16,7 +16,15 @@ const TabListGamePreview = ({ id }) => {
           <p>{gameData.short_description}</p>
         </div>
       ) : (
-        <div></div>
+        <div className="bg-gray-200 rounded-md grid place-content-center">
+          <h3 className="text-center text-2xl italic font-bold p-2">No Preview!</h3>
+          <p className="text-center p-2 grid grid-flow-col items-center">
+            <span className="material-symbols-rounded  m-3 translate-y-[0.7px]">
+              keyboard_backspace
+            </span>
+            select a game from list to preview
+          </p>
+        </div>
       )}
     </>
   );
