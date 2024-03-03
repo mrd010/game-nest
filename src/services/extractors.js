ad15d2dbd5f75d9c485743789672db31922ccc69
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 12:03:01 +0330

    Add check ability for game data exist to extractors

--- src/services/extractors.js
+++ src/services/extractors.js
@@ -9,36 +9,43 @@ export const extractRefinedGamesArray = (gameArray) => {
 export const extractGameSysReq = (gameData) => {
   let systemRequirements = {};
 
-  if (gameData.pc_requirements && gameData.pc_requirements.minimum) {
-    const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.minimum);
-    if (sysReqArray.length && sysReqArray[0].length) {
-      systemRequirements.pcMinimum = sysReqArray;
+  // if not received game data yet then return
+  if (gameData) {
+    // each if checks for availability of game for that os and then proceeds
+    if (gameData.pc_requirements && gameData.pc_requirements.minimum) {
+      const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.minimum);
+      if (sysReqArray.length && sysReqArray[0].length) {
+        systemRequirements.pcMinimum = sysReqArray;
+      }
     }
-  }
-  if (gameData.pc_requirements && gameData.pc_requirements.recommended) {
-    const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.recommended);
-    if (sysReqArray.length && sysReqArray[0].length) {
-      systemRequirements.pcRecommended = sysReqArray;
+    if (gameData.pc_requirements && gameData.pc_requirements.recommended) {
+      const sysReqArray = htmlToSysReqArray(gameData.pc_requirements.recommended);
+      if (sysReqArray.length && sysReqArray[0].length) {
+        systemRequirements.pcRecommended = sysReqArray;
+      }
     }
-  }
-  if (gameData.mac_requirements && gameData.mac_requirements.minimum) {
-    const sysReqArray = htmlToSysReqArray(gameData.mac_requirements.minimum);
-    if (sysReqArray.length && sysReqArray[0].length) {
-      systemRequirements.macMinimum = sysReqArray;
+    if (gameData.mac_requirements && gameData.mac_requirements.minimum) {
+      const sysReqArray = htmlToSysReqArray(gameData.mac_requirements.minimum);
+      if (sysReqArray.length && sysReqArray[0].length) {
+        systemRequirements.macMinimum = sysReqArray;
+      }
     }
-  }
-  if (gameData.linux_requirements && gameData.linux_requirements.minimum) {
-    const sysReqArray = htmlToSysReqArray(gameData.linux_requirements.minimum);
-    if (sysReqArray.length && sysReqArray[0].length) {
-      systemRequirements.linuxMinimum = sysReqArray;
+    if (gameData.linux_requirements && gameData.linux_requirements.minimum) {
+      const sysReqArray = htmlToSysReqArray(gameData.linux_requirements.minimum);
+      if (sysReqArray.length && sysReqArray[0].length) {
+        systemRequirements.linuxMinimum = sysReqArray;
+      }
     }
   }
 
   return systemRequirements;
 };
 
-export const extractPriorityPlatform = (platforms) => {
-  return Object.entries(platforms).find((platform) => platform[1] === true)[0];
+export const extractPriorityPlatform = (gameData) => {
+  if (gameData) {
+    return Object.entries(gameData.platforms).find((platform) => platform[1] === true)[0];
+  }
+  return null;
 };
 
 export const extractGameData = (data) => {
