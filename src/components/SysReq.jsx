a29f9806255204bd608d1f9d3c3e8c67b089a0fa
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 23:54:55 +0330

    Style sys req component

--- src/components/SysReq.jsx
+++ src/components/SysReq.jsx
@@ -1,21 +1,27 @@
 import PropTypes from 'prop-types';
+import { capitalizeWord } from '../services/utilities';
 const SysReq = ({ platform, title, systemReqData }) => {
   return (
-    <div>
-      <h4>
-        {platform === 'windows' ? '' : platform} {title}
+    <div className="grid justify-items-start gap-1 bg-gray-200 rounded-md p-2 justify-self-start">
+      <h4 className="text-base font-bold bg-gray-900 rounded-md text-gray-50 px-1">
+        {platform === 'windows' ? capitalizeWord(title) : capitalizeWord(platform)}
       </h4>
-      <ul>
+      <ul className="grid grid-cols-3 gap-1">
         {systemReqData.map((reqData, index) => (
-          <li key={index}>
-            <span>{reqData[0]}</span>
-            <span>{reqData[1]}</span>
+          <li
+            key={index}
+            className={`flex flex-col border-[1px] border-gray-900/10 px-1 rounded-sm ${index === 0 && !reqData[0].toLowerCase().includes('os') ? 'col-span-3' : ''}`}
+          >
+            <span className="font-bold text-sm">{reqData[0]}</span>
+            <span className="text-xs text-gray-500/90">{reqData[1]}</span>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
+// const Title = ({children}) => { third }
 
 SysReq.propTypes = {
   platform: PropTypes.oneOf(['windows', 'mac', 'linux']),
