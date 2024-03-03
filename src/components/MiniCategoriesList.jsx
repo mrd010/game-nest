abe6099d2efb20cceebaef7cf1b323486ef81504
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 22:55:19 +0330

    Replace file with mini categories list component

---
+++ src/components/MiniCategoriesList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+const MiniCategoriesList = ({ categoryList }) => {
+  return (
+    <>
+      {categoryList.map((cat) => (
+        <Link key={cat.id} to={`categories/${cat.id}`}>
+          {cat.description}
+        </Link>
+      ))}
+    </>
+  );
+};
+MiniCategoriesList.propTypes = {
+  categoryList: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ),
+};
+export default MiniCategoriesList;
