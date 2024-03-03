82cad52461e6d9f46aafc548ae07e00d8147a2c9
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 22:44:33 +0330

    Fix links to kebab case

--- src/components/GameDetailsCategories.jsx
+++ src/components/GameDetailsCategories.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { toKebabCase } from '../services/utilities';
 const GameDetailsCategories = ({ categoryList }) => {
   return (
     <div>
       {categoryList.map((cat) => (
-        <Link key={cat} to={cat}>
+        <Link key={cat} to={toKebabCase(cat)}>
           {cat}
         </Link>
       ))}
