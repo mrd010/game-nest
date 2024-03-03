fe710192b0e0e3b8071e3cc8c19564c89a65f5d9
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-03 11:51:08 +0330

    Create structure of sys req comp receive reqData and outputs list

--- src/components/SysReq.jsx
+++ src/components/SysReq.jsx
@@ -1,7 +1,22 @@
 import PropTypes from 'prop-types';
 const SysReq = ({ platform, title, systemReqData }) => {
-  return <div>{platform}</div>;
+  return (
+    <div>
+      <h4>
+        {platform === 'windows' ? '' : platform} {title}
+      </h4>
+      <ul>
+        {systemReqData.map((reqData, index) => (
+          <li key={index}>
+            <span>{reqData[0]}</span>
+            <span>{reqData[1]}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
 SysReq.propTypes = {
   platform: PropTypes.oneOf(['windows', 'mac', 'linux']),
   title: PropTypes.oneOf(['minimum', 'recommended']),
