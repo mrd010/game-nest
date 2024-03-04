58edf918830a9e8ee4e6f4b4d8fb9d4c89dbc128
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-04 12:07:04 +0330

    Style free Game label

--- src/components/FreeGameLabel.jsx
+++ src/components/FreeGameLabel.jsx
@@ -1,9 +1,15 @@
 import freeLabel from '../assets/img/free-label.png';
 const FreeGameLabel = () => {
   return (
-    <div>
-      <img src={freeLabel} alt="free label" />
-      <span className="uppercase font-extrabold">Free</span>
+    <div className="grid grid-cols-1 grid-rows-1 place-items-center w-24">
+      <img
+        src={freeLabel}
+        alt="free label"
+        className="col-end-1 row-end-1 translate-x-1 translate-y-1"
+      />
+      <span className="uppercase z-20 font-extrabold text-sm col-end-1 row-end-1 text-gray-900">
+        Free
+      </span>
     </div>
   );
 };
