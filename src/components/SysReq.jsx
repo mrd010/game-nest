07363eddea080f071b7c31c4ba1d5bde05eed5a9
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-04 12:07:25 +0330

    Fix bugs

--- src/components/SysReq.jsx
+++ src/components/SysReq.jsx
@@ -6,16 +6,18 @@ const SysReq = ({ platform, title, systemReqData }) => {
       <h4 className="text-base font-bold justify-self-start bg-gray-900 rounded-md text-gray-50 px-1">
         {platform === 'windows' ? capitalizeWord(title) : capitalizeWord(platform)}
       </h4>
-      <ul className="grid grid-cols-3 gap-1">
-        {systemReqData.map((reqData, index) => (
-          <li
-            key={index}
-            className={`flex flex-col border-[1px] border-gray-900/10 px-1 rounded-sm ${index === 0 && !reqData[0].toLowerCase().includes('os') ? 'col-span-3' : ''}`}
-          >
-            <span className="font-bold text-sm">{reqData[0]}</span>
-            <span className="text-xs text-gray-500/90 break-words">{reqData[1]}</span>
-          </li>
-        ))}
+      <ul className="grid grid-cols-3 gap-1 h-full">
+        {systemReqData.map((reqData, index) =>
+          reqData.length === 2 ? (
+            <li
+              key={index}
+              className={`flex flex-col border-[1px] border-gray-900/10 px-1 rounded-sm ${index === 0 && !reqData[0].toLowerCase().includes('os') ? 'col-span-3 border-none' : ''}`}
+            >
+              <span className="font-bold text-sm">{reqData[0]}</span>
+              <span className="text-xs text-gray-500/90 break-words">{reqData[1]}</span>
+            </li>
+          ) : null
+        )}
       </ul>
     </div>
   );
