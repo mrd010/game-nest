82b3a2816c514b55ab1d48114fe408356ccbee30
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-04 20:29:47 +0330

    Make sys req grid cell to auto arrange

--- src/components/SysReq.jsx
+++ src/components/SysReq.jsx
@@ -6,15 +6,15 @@ const SysReq = ({ platform, title, systemReqData }) => {
       <h4 className="text-base font-bold justify-self-start bg-gray-900 rounded-md text-gray-50 px-1">
         {platform === 'windows' ? capitalizeWord(title) : capitalizeWord(platform)}
       </h4>
-      <ul className="grid grid-cols-3 gap-1 h-full">
+      <ul className="grid grid-cols-4 grid-flow-dense gap-1 h-full">
         {systemReqData.map((reqData, index) =>
           reqData.length === 2 ? (
             <li
               key={index}
-              className={`flex flex-col border-[1px] border-gray-900/10 px-1 rounded-sm ${index === 0 && !reqData[0].toLowerCase().includes('os') ? 'col-span-3 border-none' : ''}`}
+              className={`flex flex-col border-[1px] border-gray-900/10 px-1 rounded-sm ${index === 0 && !reqData[0].toLowerCase().includes('os') ? 'col-span-4 border-none' : ''} ${reqData[1].length > 80 ? 'col-span-4' : reqData[1].length > 50 ? 'col-span-3' : reqData[1].length > 25 ? 'col-span-2' : ''}`}
             >
               <span className="font-bold text-sm">{reqData[0]}</span>
-              <span className="text-xs text-gray-500/90 break-words">{reqData[1]}</span>
+              <span className={`text-xs text-gray-500/90 break-words `}>{reqData[1]}</span>
             </li>
           ) : null
         )}
