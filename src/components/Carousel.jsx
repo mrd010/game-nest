ee859d2ca78fb8722a3b73ad7d3f5d63531d3938
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-05 19:56:15 +0330

    Fix scrolling step by adding steps and decrease threshold

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import ActionButton from './ActionButton';
 import { useEffect, useRef, useState } from 'react';
 
-const Carousel = ({ title, itemWidth, children }) => {
+const Carousel = ({ title, itemWidth, steps, children }) => {
   // carousel component which scrolls horizontally through its children with left and right buttons
 
   // item currently is showing in left of carousel
@@ -10,9 +10,9 @@ const Carousel = ({ title, itemWidth, children }) => {
   const carousel = useRef(null);
 
   const handleScrollChange = (direction) => {
-    // when pressed arrow buttons change position between 0 and max items - 3
-    // note: 3 is number of items scrolling with each scroll
-    setCurrentPosition((p) => Math.min(Math.max(0, p + direction), children.length - 3));
+    // when pressed arrow buttons change position between 0 and max items - steps -1
+    // note: steps is number of items scrolling with each scroll
+    setCurrentPosition((p) => Math.min(Math.max(0, p + direction), children.length - steps - 1));
   };
 
   useEffect(() => {
@@ -21,23 +21,23 @@ const Carousel = ({ title, itemWidth, children }) => {
   }, [itemWidth, currentPosition]);
   return (
     <div
-      className={`grid grid-cols-[1fr_auto] grid-row-2 relative gap-5 ${currentPosition > 0 ? 'before:content-[" "] before:absolute before:bottom-2 before:left-0 before:z-50 before:h-[calc(100%_-_60px)] before:w-10 before:bg-gradient-to-l before:from-transparent before:to-gray-50 before:to-80%' : ''}${currentPosition < children.length - 3 ? 'after:content-[" "] after:w-8 after:right-0 after:bottom-2 after:h-[calc(100%_-_60px)] after:bg-gradient-to-r after:to-80% after:from-transparent after:to-gray-50 after:absolute after:z-50' : ''}`}
+      className={`grid grid-cols-[1fr_auto] grid-row-2 relative gap-5 ${currentPosition > 0 ? 'before:content-[" "] before:absolute before:bottom-2 before:left-0 before:z-50 before:h-[calc(100%_-_60px)] before:w-10 before:bg-gradient-to-l before:from-transparent before:to-gray-50 before:to-80%' : ''}${currentPosition < children.length - steps - 1 ? 'after:content-[" "] after:w-8 after:right-0 after:bottom-2 after:h-[calc(100%_-_60px)] after:bg-gradient-to-r after:to-80% after:from-transparent after:to-gray-50 after:absolute after:z-50' : ''}`}
     >
       <h2 className="text-2xl font-bold border-b-[1px] pb-2 border-gray-900/50">{title}</h2>
       <div className="flex flex-row gap-2 justify-self-end self-end">
-        <ActionButton isDisabled={currentPosition <= 0} onClick={() => handleScrollChange(-3)}>
+        <ActionButton isDisabled={currentPosition <= 0} onClick={() => handleScrollChange(-steps)}>
           <span className="material-symbols-rounded">arrow_back</span>
         </ActionButton>
         <ActionButton
-          isDisabled={currentPosition >= children.length - 3}
-          onClick={() => handleScrollChange(+3)}
+          isDisabled={currentPosition >= children.length - steps - 1}
+          onClick={() => handleScrollChange(+steps)}
         >
           <span className="material-symbols-rounded">arrow_forward</span>
         </ActionButton>
       </div>
       {children && (
         <div
-          className="grid grid-flow-col auto-cols-max gap-4 scroll-smooth col-span-2 overflow-hidden pb-2  carousel"
+          className="grid grid-flow-col auto-cols-max gap-4 scroll-smooth col-span-2 overflow-hidden pb-2 carousel"
           ref={carousel}
         >
           {children}
@@ -49,6 +49,7 @@ const Carousel = ({ title, itemWidth, children }) => {
 Carousel.propTypes = {
   title: PropTypes.string,
   itemWidth: PropTypes.number.isRequired,
+  steps: PropTypes.number.isRequired,
   children: PropTypes.oneOfType(PropTypes.node, PropTypes.arrayOf(PropTypes.node)),
 };
 export default Carousel;
