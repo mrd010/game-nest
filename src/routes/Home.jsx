3a0a3334746f7fda0b8dbef85bb42377729e9a9f
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-05 19:57:22 +0330

    Add important games and news section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,10 +2,11 @@ import { useLoaderData } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import RecommendedCard from '../components/RecommendedCard';
 import TabbedCategories from '../components/TabbedCategories';
-import { extractRefinedGamesArray } from '../services/extractors';
+import { extractImportantGameIds, extractRefinedGamesArray } from '../services/extractors';
+import NewsSection from '../components/NewsSection';
 
 const Home = () => {
-  const { recommendedGames, featuredCategories, trailers, importantGamesIds } = useLoaderData();
+  const { recommendedGames, featuredCategories, trailers } = useLoaderData();
 
   // remove duplicate games from data
   const refinedCategoriesData = {
@@ -17,19 +18,25 @@ const Home = () => {
     top_sellers: extractRefinedGamesArray(featuredCategories.top_sellers.items),
   };
 
-  console.log(importantGamesIds);
+  const importantGamesIds = extractImportantGameIds(
+    recommendedGames,
+    refinedCategoriesData.top_sellers
+  );
 
   return (
     <div className="flex flex-col flex-nowrap gap-10">
       <section className="my-5">
         {recommendedGames.length && (
-          <Carousel title="Recommended Games" itemWidth={290}>
+          <Carousel title="Recommended Games" itemWidth={290} steps={3}>
             {recommendedGames.map((game) => (
               // create carousel with recommended games (games released recently and have high meta score)
               <RecommendedCard key={game.steamAppID} {...game}></RecommendedCard>
             ))}
           </Carousel>
         )}
+      </section>
+      <section className="h-[280px]">
+        <NewsSection importantGameIds={importantGamesIds}></NewsSection>
       </section>
       <section className="border-t-[1px] border-gray-900/50 py-1">
         <TabbedCategories categoriesData={refinedCategoriesData}></TabbedCategories>
