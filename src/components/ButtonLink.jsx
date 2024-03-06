3e9c06ce31b703cccc1bb8e7cb2cde47206868d6
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-07 00:35:24 +0330

    Add custom styles proprty

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-const ButtonLink = ({ text, link }) => {
+const ButtonLink = ({ text, link, className = '' }) => {
   // simple Link with schematics of button
 
   return (
     <Link
-      className="font-semibold text-gray-950 bg-yellow-400 focus:bg-yellow-300 hover:bg-yellow-300 shadow-sm py-2 px-8 rounded-md transition-colors"
+      className={`font-semibold text-gray-950 bg-yellow-400 focus:bg-yellow-300 hover:bg-yellow-300 shadow-sm py-2 px-8 rounded-md transition-colors ${className}`}
       to={link}
     >
       {text}
@@ -15,5 +15,6 @@ const ButtonLink = ({ text, link }) => {
 ButtonLink.propTypes = {
   text: PropTypes.string.isRequired,
   link: PropTypes.string,
+  className: PropTypes.string,
 };
 export default ButtonLink;
