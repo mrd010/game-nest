e2b4a1333a5063deb99198c971ef265fe7694388
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-06 23:38:07 +0330

    Fix bug where empty news items fulfilled in promise array

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -6,38 +6,52 @@ import { getNews } from '../services/dataFetchers';
 const NewsSection = ({ importantGameIds }) => {
   // container for game news
   const [newsData, setNewsData] = useState(null);
+  const [error, setError] = useState(false);
 
   // stringify array for not triggering deps
   const stringifiedIds = JSON.stringify(importantGameIds);
 
   useEffect(() => {
+    let ignore = false;
     const fetchNewsData = async () => {
-      const fetchedData = await getNews(stringifiedIds);
+      try {
+        const fetchedData = await getNews(stringifiedIds);
+        // sort news by date
 
-      fetchedData.sort((a, b) => b.date - a.date);
-      setNewsData(fetchedData);
+        fetchedData.sort((a, b) => b.date - a.date);
+
+        if (!ignore) {
+          setNewsData(fetchedData);
+        }
+      } catch (error) {
+        console.log(error.message);
+        setError(true);
+      }
     };
 
     fetchNewsData();
+    return () => (ignore = true);
   }, [stringifiedIds]);
 
   return (
     <>
-      <Carousel itemWidth={230} steps={4} title="News">
-        {newsData
-          ? newsData.map(
-              (news) =>
-                news.title.length > 20 && (
-                  <News
-                    key={news.appid}
-                    appId={news.appid}
-                    title={news.title}
-                    url={news.url}
-                  ></News>
-                )
-            )
-          : [...Array(6)].map((v, index) => <News key={index}></News>)}
-      </Carousel>
+      {!error && (
+        <Carousel itemWidth={230} steps={4} title="News">
+          {newsData
+            ? newsData.map(
+                (news) =>
+                  news.title.length > 20 && (
+                    <News
+                      key={news.appid}
+                      appId={news.appid}
+                      title={news.title}
+                      url={news.url}
+                    ></News>
+                  )
+              )
+            : [...Array(6)].map((v, index) => <News key={index}></News>)}
+        </Carousel>
+      )}
     </>
   );
 };
