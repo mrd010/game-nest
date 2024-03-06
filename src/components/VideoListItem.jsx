c5a3f4592baae82a13439d0186fb6f1245d24e86
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-06 20:22:53 +0330

    Add trailers list item

---
+++ src/components/VideoListItem.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+const VideoListItem = ({ thumbnail, name }) => {
+  return <div></div>;
+};
+VideoListItem.propTypes = {
+  thumbnail: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+export default VideoListItem;
