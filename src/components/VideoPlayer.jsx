e7527d522839393a0ee869f1e100eab6ae437df7
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-08 11:33:59 +0330

    Add some comments

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -15,14 +15,20 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { secondsToMinutesPlus } from '../services/utilities';
 
 const VideoPlayer = ({ lqUrl, hqUrl, previewImage }) => {
+  // video player with custom controls
   const [isStarted, setIsStarted] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [highQuality, setHighQuality] = useState(false);
   const [volume, setVolume] = useState(0.8);
   const [isMuted, setIsMuted] = useState(false);
+  // using percent for seeking and using seconds for displaying time
+  // seconds format
   const [timePlayed, setTimePlayed] = useState(0);
+  // percent format
   const [percentPlayed, setPercentPlayed] = useState(0);
+  // seeking state is when we are holding slider for seeking video time
   const [isSeeking, setIsSeeking] = useState(false);
+  // when video is buffering
   const [isLoading, setIsLoading] = useState(false);
 
   const player = useRef(null);
@@ -60,21 +66,27 @@ const VideoPlayer = ({ lqUrl, hqUrl, previewImage }) => {
           onPause={() => handlePlayPause(false)}
           onBuffer={() => setIsLoading(true)}
           onBufferEnd={() => setIsLoading(false)}
+          // on video progress get and set played video time in percent and in seconds
+
           onProgress={({ played, playedSeconds }) => {
+            // if we are not in seek mode we don't want to update video
             if (!isSeeking) {
               setTimePlayed(playedSeconds);
               setPercentPlayed(played);
             }
           }}
+          // usually wont need. its just for when changing quality videos we want to resume from current time
           onStart={() => player.current.seekTo(percentPlayed)}
         ></ReactPlayer>
       </div>
       {/* controls */}
       {isLoading && (
+        // video loader
         <div className="absolute w-full h-full grid place-items-center">
           <div className="video-loader"></div>
         </div>
       )}
+      {/* only show controls when video started */}
       {isStarted && (
         <div className="absolute bottom-0 w-full flex flex-col p-2 video-controls transition-opacity opacity-75 group-hover:opacity-100 bg-gray-900/50">
           <div className="px-2 video-track bg-transparent flex flex-nowrap gap-4 items-center">
@@ -82,19 +94,24 @@ const VideoPlayer = ({ lqUrl, hqUrl, previewImage }) => {
             <input
               type="range"
               className="w-full bg-transparent range transition-opacity hover:opacity-100 opacity-70"
-              value={Math.floor(percentPlayed * 100)}
+              value={Math.ceil(percentPlayed * 100)}
               onMouseDown={() => setIsSeeking(true)}
               onMouseUp={(e) => {
                 setIsSeeking(false);
+                // when finish seeking move video to selected percent played
                 player.current.seekTo(parseFloat(e.target.value / 100));
               }}
               onChange={(e) => {
+                // save percent time while seeking and move video time to this time when seeking ends
                 setPercentPlayed(parseFloat(e.target.value / 100));
               }}
             />
             {/* current time */}
             <span className="font-bold text-lg">
-              {secondsToMinutesPlus(Math.floor(timePlayed))}
+              {
+                // convert seconds to 00:00 format
+                secondsToMinutesPlus(Math.floor(timePlayed))
+              }
             </span>
           </div>
           <div className="flex flex-nowrap gap-4">
@@ -119,6 +136,7 @@ const VideoPlayer = ({ lqUrl, hqUrl, previewImage }) => {
                 type="range"
                 value={volume * 100}
                 onChange={(e) => {
+                  // on volume change get out of muted state
                   setIsMuted(false);
                   setVolume(e.target.value / 100);
                 }}
