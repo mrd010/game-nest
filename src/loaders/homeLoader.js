b328dd827c52232a177f3fc270c40e02c7432551
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-08 13:12:03 +0330

    Add best deals getter

--- src/loaders/homeLoader.js
+++ src/loaders/homeLoader.js
@@ -1,8 +1,14 @@
-import { getFeaturedCategories, getRecommendedGames, getTrailers } from '../services/dataFetchers';
+import {
+  getBestDeals,
+  getFeaturedCategories,
+  getRecommendedGames,
+  getTrailers,
+} from '../services/dataFetchers';
 import getUserLanguage from '../services/userLocaleServices';
 
 // recommended games should be above this threshold
 const recommendedGamesMinScore = 80;
+const bestDealsMinScore = 50;
 // get user lang
 const userLanguage = getUserLanguage();
 
@@ -12,6 +18,7 @@ export const homeLoader = async () => {
     getRecommendedGames(recommendedGamesMinScore),
     getFeaturedCategories(userLanguage),
     getTrailers(userLanguage),
+    getBestDeals(bestDealsMinScore, 4),
   ];
 
   const homeData = await Promise.all(dataPromises).then((data) => {
@@ -19,6 +26,7 @@ export const homeLoader = async () => {
       recommendedGames: data[0],
       featuredCategories: data[1],
       trailers: data[2],
+      bestDeals: data[3],
     };
   });
   return homeData;
