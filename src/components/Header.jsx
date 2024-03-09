82118baeba8cf44077d5399575a49d098e035cd3
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-09 18:03:14 +0330

    Fix styles of header links for hover effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,26 +8,24 @@ const Header = ({ categories }) => {
   // top header of app
   return (
     <header className="bg-gray-950 text-gray-50 min-w-min">
-      <div className="grid grid-cols-[auto_auto_1fr_auto] items-center p-3 mx-auto w-[1280px] justify-between gap-8">
-        <div className="mr-10">
+      <div className="grid grid-cols-[auto_auto_1fr_auto] items-center content-center mx-auto w-[1280px] justify-between gap-8">
+        <div className="mr-10 p-4">
           <h1>
             <Link className="text-4xl font-extrabold">
               Game<span className="text-yellow-400">Nest</span>
             </Link>
           </h1>
         </div>
-        <nav className="grid grid-cols-3 items-center gap-5">
+        <nav className="grid grid-cols-3 gap-5 h-full">
           <HeaderNavLink text="Home" link="home"></HeaderNavLink>
           <HeaderNavLink text="Games" link="games">
-            <div>
-              {categories.map((cat) =>
-                cat.id === cat.name ? (
-                  <Link key={cat.id} to={`/games/${cat.id}`}>
-                    {cat.name}
-                  </Link>
-                ) : null
-              )}
-            </div>
+            {categories.map((cat) =>
+              cat.id === cat.name ? (
+                <Link key={cat.id} to={`/games/${cat.id}`}>
+                  {cat.name}
+                </Link>
+              ) : null
+            )}
           </HeaderNavLink>
         </nav>
         <div>
--- src/components/HeaderNavLink.jsx
+++ src/components/HeaderNavLink.jsx
@@ -1,17 +1,37 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 
 const HeaderNavLink = ({ text, link, children }) => {
+  const [opened, setOpened] = useState(false);
+  const timer = useRef(null);
   // links in header of page which navigates to different main pages of app
   return (
-    <div>
+    <div
+      className="relative grid items-center"
+      onMouseLeave={() => {
+        clearTimeout(timer.current);
+        setOpened(false);
+      }}
+    >
       <Link
         to={link}
-        className="font-semibold hover:bg-gray-50 hover:text py-2 px-4 hover:text-gray-950 rounded-md transition-colors"
+        className={`font-semibold py-2 px-4 rounded-md transition-colors hover:bg-gray-50 hover:text-gray-950 ${opened ? 'bg-gray-50 text-gray-950' : 'bg-transparent text-gray-50'}`}
+        onMouseEnter={() => {
+          timer.current = setTimeout(() => {
+            setOpened(true);
+          }, 300);
+        }}
       >
         {text}
       </Link>
-      {children && <div>{children}</div>}
+      {children && (
+        <div
+          className={`absolute z-10 w-max p-2 text-gray-800 bg-gray-100 shadow-2xl rounded-b border-x-[3px] top-full left-0 grid grid-flow-row transition-opacity ${opened ? 'opacity-100' : 'opacity-0'}`}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
