cfaae5317c379c10cf4110d54ae280b28e94dd4b
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-10 00:44:49 +0330

    Add data fixers and base structure of page

--- src/routes/NewReleasedGames.jsx
+++ src/routes/NewReleasedGames.jsx
@@ -1,4 +1,38 @@
+import { useLoaderData } from 'react-router-dom';
+import HomeSectionTitle from '../components/HomeSectionTitle';
+import GamesAlbum from '../components/GamesAlbum';
+import { extractJustGames } from '../services/extractors';
+
 const NewReleasedGames = () => {
-  return <div>NewReleasedGames</div>;
+  const { viewall, topsellers, specials, dlc } = useLoaderData();
+
+  const allGames =
+    viewall && viewall.items && viewall.items.length > 0 ? extractJustGames(viewall.items) : null;
+  const topSellerGames =
+    topsellers && topsellers.items && topsellers.items.length > 0
+      ? extractJustGames(topsellers.items)
+      : null;
+  const specialGames =
+    specials && specials.items && specials.items.length > 0
+      ? extractJustGames(specials.items)
+      : null;
+  const gameDLCS = dlc && dlc.items && dlc.items.length > 0 ? extractJustGames(dlc.items) : null;
+
+  return (
+    <div className="space-y-20">
+      <header>
+        <h1 className="text-4xl my-2 font-extrabold">What Games to Play Right Now</h1>
+        <p className="text-lg">
+          Find newly Released and Top Seller games for PC and filter by genre.
+        </p>
+      </header>
+      {viewall && (
+        <section>
+          <HomeSectionTitle>New Releases</HomeSectionTitle>
+          <GamesAlbum gameIds={allGames} itemsPerPage={10}></GamesAlbum>
+        </section>
+      )}
+    </div>
+  );
 };
 export default NewReleasedGames;
