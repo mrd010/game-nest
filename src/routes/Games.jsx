6fdadc6330a8233db8276d199ba02d9291cdc287
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-09 19:39:08 +0330

    Add side nav to games page

--- src/routes/Games.jsx
+++ src/routes/Games.jsx
@@ -1,4 +1,14 @@
+import { Outlet, useOutletContext } from 'react-router-dom';
+import CategoriesSideNav from '../components/CategoriesSideNav';
+
 const Games = () => {
-  return <div>Games</div>;
+  const categories = useOutletContext();
+  console.log(categories);
+  return (
+    <div>
+      <CategoriesSideNav categoryList={categories}></CategoriesSideNav>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 export default Games;
