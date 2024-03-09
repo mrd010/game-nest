e434b115fc49bd0140366d9db36ebc4c99b04a15
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-09 14:01:28 +0330

    Remove deals page

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,17 +7,17 @@ import {
 import Layout from './Layout';
 import Home from './routes/Home';
 import Games from './routes/Games';
-import Deals from './routes/Deals';
 import { homeLoader } from './loaders/homeLoader';
+
+import { appLoader } from './loaders/appLoader';
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout />} loader={appLoader}>
         <Route index element={<Home />} loader={homeLoader}></Route>
         <Route path="home" element={<Home />} loader={homeLoader}></Route>
         <Route path="games" element={<Games />}></Route>
-        <Route path="deals" element={<Deals />}></Route>
       </Route>
     )
   );
