2fcbe07dad26897122c925a76091d540cceece33
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-10 14:54:15 +0330

    Create base games card component in games browser

--- src/components/GamesAlbumCard.jsx
+++ src/components/GamesAlbumCard.jsx
@@ -1,8 +1,35 @@
 import PropTypes from 'prop-types';
-const GamesAlbumCard = ({ id }) => {
-  return <div>{id}</div>;
+import ContentLoader from './ContentLoader';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { steamHeaderImage } from '../services/utilities';
+
+const GamesAlbumCard = ({ gameData }) => {
+  return (
+    <div className="grid gap-2">
+      {gameData ? (
+        <>
+          <LazyLoadImage src={steamHeaderImage(gameData.appid)}></LazyLoadImage>
+          <h4>{gameData.name}</h4>
+          <p>{gameData.strSnippet}</p>
+          <p>{gameData.rgDevelopers[0].name}</p>
+          <p>{gameData.rgPublishers[0].name}</p>
+        </>
+      ) : (
+        <>
+          <ContentLoader size="100px"></ContentLoader>
+          <ContentLoader size="20px"></ContentLoader>
+        </>
+      )}
+    </div>
+  );
 };
 GamesAlbumCard.propTypes = {
-  id: PropTypes.number.isRequired,
+  gameData: PropTypes.shape({
+    appid: PropTypes.string,
+    name: PropTypes.string,
+    strSnippet: PropTypes.string,
+    rgDevelopers: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })),
+    rgPublishers: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })),
+  }),
 };
 export default GamesAlbumCard;
