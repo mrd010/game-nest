1e749b23b314ba88ad339f9712c7fcc08f0f8893
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-10 23:09:47 +0330

    Add styles for game card

--- src/components/GamesAlbumCard.jsx
+++ src/components/GamesAlbumCard.jsx
@@ -1,25 +1,40 @@
 import PropTypes from 'prop-types';
-import ContentLoader from './ContentLoader';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { steamHeaderImage } from '../services/utilities';
+import ContentLoader from './ContentLoader';
+import PubDevRow from './PubDevRow';
+import { Link } from 'react-router-dom';
 
 const GamesAlbumCard = ({ gameData }) => {
   return (
-    <div className="grid gap-2">
-      {gameData ? (
-        <>
-          <LazyLoadImage src={steamHeaderImage(gameData.appid)}></LazyLoadImage>
-          <h4>{gameData.name}</h4>
-          <p>{gameData.strSnippet}</p>
-          <p>{gameData.rgDevelopers[0].name}</p>
-          <p>{gameData.rgPublishers[0].name}</p>
-        </>
-      ) : (
-        <>
-          <ContentLoader size="100px"></ContentLoader>
-          <ContentLoader size="20px"></ContentLoader>
-        </>
-      )}
+    <div className="grid grid-cols-[auto_minmax(0,2fr)] gap-5 h-min items-center hover:opacity-85 transition-opacity">
+      <Link to={`/games/${gameData.appid}`} className="">
+        <LazyLoadImage
+          src={steamHeaderImage(gameData.appid)}
+          placeholder={<div className="content-loader h-full w-full"></div>}
+          className="shadow-md contrast-[110%]"
+        ></LazyLoadImage>
+      </Link>
+      <div className="grid grid-rows-[auto_minmax(0,1fr)_auto] gap-1 py-4 h-full">
+        <div>
+          <h4 className="text-xl font-bold line-clamp-1 hover:text-yellow-600 underline">
+            <Link to={`/games/${gameData.appid}`}>{gameData.name}</Link>
+          </h4>
+        </div>
+        <div className="overflow-hidden">
+          <p className="text-gray-600/85 line-clamp-3">{gameData.strSnippet}</p>
+        </div>
+        <div className="grid grid-cols-2">
+          <PubDevRow>
+            <PubDevRow.Title>Publisher</PubDevRow.Title>
+            <PubDevRow.Value>{gameData.rgPublishers[0].name}</PubDevRow.Value>
+          </PubDevRow>
+          <PubDevRow>
+            <PubDevRow.Title>Developers</PubDevRow.Title>
+            <PubDevRow.Value>{gameData.rgDevelopers[0].name}</PubDevRow.Value>
+          </PubDevRow>
+        </div>
+      </div>
     </div>
   );
 };
@@ -30,6 +45,6 @@ GamesAlbumCard.propTypes = {
     strSnippet: PropTypes.string,
     rgDevelopers: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })),
     rgPublishers: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })),
-  }),
+  }).isRequired,
 };
 export default GamesAlbumCard;
