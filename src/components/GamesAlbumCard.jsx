093f98318a8cf5296014ab34fb874b16688671f1
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-11 00:27:47 +0330

    Set animations to work only when image loaded

--- src/components/GamesAlbumCard.jsx
+++ src/components/GamesAlbumCard.jsx
@@ -3,19 +3,24 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { steamHeaderImage } from '../services/utilities';
 import PubDevRow from './PubDevRow';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const GamesAlbumCard = ({ gameData }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <Link
       to={`/games/${gameData.appid}`}
-      className="group grid grid-cols-[auto_minmax(0,2fr)] gap-2 h-min items-center transition-opacity drop-shadow-md hover:drop-shadow-xl rounded-lg bg-slate-50 overflow-hidden"
+      className="group grid grid-cols-[auto_minmax(0,2fr)] gap-2 h-min items-center transition-opacity drop-shadow-md hover:drop-shadow-xl rounded-lg bg-slate-50 overflow-hidden min-h-[215px]"
     >
-      <div className="absolute left-0 -top-12 w-full h-[420px] bg-gradient-to-t from-amber-500 to-yellow-300 -z-50 transition-transform duration-300 rotate-[62deg] -translate-x-[400px] group-hover:-translate-x-10"></div>
+      {imageLoaded && (
+        <div className="absolute left-0 -top-12 w-full h-[420px] bg-gradient-to-t from-amber-500 to-yellow-300 -z-50 transition-transform duration-300 rotate-[62deg] -translate-x-[400px] group-hover:-translate-x-10"></div>
+      )}
       <div className="">
         <LazyLoadImage
           src={steamHeaderImage(gameData.appid)}
           placeholder={<div className="content-loader h-full w-full"></div>}
-          className="shadow-md contrast-[110%] rounded-l-lg transition-transform group-hover:scale-90 group-hover:rounded-lg duration-300"
+          className={`shadow-md contrast-[110%] rounded-l-lg transition-transform duration-300 ${imageLoaded ? 'group-hover:scale-90 group-hover:rounded-lg' : ''}`}
+          onLoad={() => setImageLoaded(true)}
         ></LazyLoadImage>
       </div>
       <div className="grid grid-rows-[auto_minmax(0,1fr)_auto] gap-1 p-4 h-full drop-shadow-md">
