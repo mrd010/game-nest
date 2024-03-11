94128c6cc3a0e50c63253ff94fb00a817b83367f
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-12 00:06:05 +0330

    Add base section for each category in genre data

--- src/routes/GenreGames.jsx
+++ src/routes/GenreGames.jsx
@@ -1,4 +1,32 @@
+import { useLoaderData } from 'react-router-dom';
+import { extractJustGames } from '../services/extractors';
+import GamesInGenreSection from '../components/GamesInGenreSection';
+
 const GenreGames = () => {
-  return <div>GenreGames</div>;
+  const { id, name, data } = useLoaderData();
+
+  console.log(data);
+  return (
+    <div>
+      <header>
+        <h1 className="text-5xl my-2 font-extrabold">{name} Games</h1>
+        <p className="text-lg">
+          Find newly Released, Top Seller and Upcoming games in {name} genre
+        </p>
+      </header>
+      {data &&
+        Object.values(data).map(
+          (categoryData) =>
+            categoryData.items &&
+            categoryData.items.length > 0 && (
+              <GamesInGenreSection
+                key={categoryData.name}
+                name={categoryData.name}
+                items={extractJustGames(categoryData.items)}
+              ></GamesInGenreSection>
+            )
+        )}
+    </div>
+  );
 };
 export default GenreGames;
