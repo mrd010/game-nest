84ae395dbe5a7a959cb94d509386613f0289736c
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-11 13:37:10 +0330

    Make tab menu a component Tab button now is merged into tab menu comp

---
+++ src/components/TabMenu.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+const TabMenu = ({ tabsList, tabWidth = 128, selectedTabIndex, onTabChange }) => {
+  return (
+    <div className={`flex flex-row flex-nowrap relative`}>
+      {/* indicator */}
+      <div
+        className="h-1 bg-yellow-500 rounded-t-sm absolute bottom-0 left-0 transition-transform"
+        style={{
+          width: `${tabWidth}px`,
+          transform: `translate(${tabWidth * selectedTabIndex}px,0)`,
+        }}
+      ></div>
+      {/* tabs */}
+      {tabsList.map((tab, index) => (
+        <button
+          key={index}
+          onClick={() => onTabChange(index)}
+          className={`px-1 py-3 transition-opacity ${selectedTabIndex === index ? 'opacity-100 font-bold' : 'opacity-40'}`}
+          style={{ width: `${tabWidth}px` }}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+};
+TabMenu.propTypes = {
+  tabsList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tabWidth: PropTypes.number,
+  selectedTabIndex: PropTypes.number.isRequired,
+  onTabChange: PropTypes.func.isRequired,
+};
+export default TabMenu;
