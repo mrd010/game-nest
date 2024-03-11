9da9b496f1aa55f20b05350cef7d84387367bd75
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-11 13:37:34 +0330

    Change tab menus to new component

--- src/components/TabbedCategories.jsx
+++ src/components/TabbedCategories.jsx
@@ -1,30 +1,30 @@
 import PropTypes from 'prop-types';
-import TabButton from './TabButton';
 import { useState } from 'react';
 import GamesListRow from './GamesListRow';
 import PageNavNumbers from './PageNavNumbers';
 import TabListPreviewPanel from '../components/TabListGamePreview';
+import TabMenu from './TabMenu';
+
 const TabbedCategories = ({ categoriesData }) => {
   // tabbed list with tabs for each key in categoriesData
 
   // currently active tab in featured categories
-  const [selectedTab, setSelectedTab] = useState('new_releases');
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
   // pagination for each category with number of items greater than 10
   const [pageNumber, setPageNumber] = useState(0);
 
   // control active game (selected by user) in featured categories (coming soon, new releases, top sellers)
   const [selectedGameInCats, setSelectedGameInCats] = useState(null);
 
-  // number version of selected tab (1==new_releases,2===top_sellers,3==coming_soon)
-  const tabIndex = selectedTab === 'new_releases' ? 0 : selectedTab === 'top_sellers' ? 1 : 2;
-
   // max number of pages for active category
-  const numberOfPages = Math.ceil(categoriesData[selectedTab].length / 10);
+  const categoryIds = ['new_releases', 'top_sellers', 'coming_soon'];
+  const numberOfPages = Math.ceil(categoriesData[categoryIds[selectedTabIndex]].length / 10);
 
-  const handleTabChange = (tabName) => {
-    if (tabName !== selectedTab) {
+  // when change tabs reset showing page to first page
+  const handleTabChange = (tabIndex) => {
+    if (tabIndex !== selectedTabIndex) {
       setPageNumber(0);
-      setSelectedTab(tabName);
+      setSelectedTabIndex(tabIndex);
     }
   };
 
@@ -38,34 +38,17 @@ const TabbedCategories = ({ categoriesData }) => {
 
   return (
     <div className="grid grid-flow-row gap-2">
-      <div
-        className={`flex relative after:content-[' '] after:h-1 after:rounded-sm after:bg-yellow-500 after:w-32 after:absolute after:bottom-0 after:transition-transform ${tabIndex === 1 ? 'after:translate-x-32' : tabIndex === 2 ? 'after:translate-x-64' : ''}`}
-      >
-        <TabButton
-          onSelect={() => handleTabChange('new_releases')}
-          isSelected={selectedTab === 'new_releases'}
-        >
-          New Releases
-        </TabButton>
-        <TabButton
-          onSelect={() => handleTabChange('top_sellers')}
-          isSelected={selectedTab === 'top_sellers'}
-        >
-          Top Sellers
-        </TabButton>
-        <TabButton
-          onSelect={() => handleTabChange('coming_soon')}
-          isSelected={selectedTab === 'coming_soon'}
-        >
-          Coming Soon
-        </TabButton>
-      </div>
+      <TabMenu
+        selectedTabIndex={selectedTabIndex}
+        tabsList={['New Releases', 'Top Sellers', 'Coming Soon']}
+        onTabChange={handleTabChange}
+      ></TabMenu>
       <div className="flex flex-nowrap gap-12 w-full h-[780px]">
         <div className="relative grow">
           <div className={`flex flex-col group flex-nowrap divide-y-2 border-gray-950/50`}>
             {
               // divide items in pages and show active page items
-              categoriesData[selectedTab]
+              categoriesData[categoryIds[selectedTabIndex]]
                 .slice(pageNumber * 10, pageNumber * 10 + 10)
                 .map((gameData) => (
                   <GamesListRow
