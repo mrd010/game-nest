0fb20ee767c6dfba4408d9300fa09696141981cf
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-12 23:55:16 +0330

    Make side nav sticky

--- src/components/CategoriesSideNav.jsx
+++ src/components/CategoriesSideNav.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 const CategoriesSideNav = ({ categoryList }) => {
   return (
-    <nav className="w-[300px]">
-      <ul className="divide-y-2">
+    <nav className={`w-[300px]`}>
+      <ul className="divide-y-2  sticky top-0">
         {/* main nav link (default page) */}
         <li className={`text-xl px-6 hover:text-yellow-600  `}>
           <NavLink
@@ -15,13 +15,12 @@ const CategoriesSideNav = ({ categoryList }) => {
         </li>
         {/* categories nav link */}
         {categoryList.map((category) => (
-          <li
-            key={category.id}
-            className="px-6 hover:bg-gradient-to-r from-transparent to-transparent via-gray-500/10 via-15%"
-          >
+          <li key={category.id}>
             <NavLink
               to={category.id}
-              className={`text-lg w-full inline-block py-2 transition-transform text-gray-800/75`}
+              className={({ isActive, isPending, isTransitioning }) =>
+                `text-lg w-full px-6 inline-block py-1 transition-[padding] text-gray-800/75 hover:bg-gradient-to-r from-transparent to-transparent via-15% ${isActive ? 'via-yellow-300/80 bg-gradient-to-r font-bold pl-10' : isPending || isTransitioning ? 'via-gray-400/30 bg-gradient-to-r font-bold' : 'via-gray-500/10'}`
+              }
             >
               {category.name}
             </NavLink>
