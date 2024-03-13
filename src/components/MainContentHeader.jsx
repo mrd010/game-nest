f007749e6e0a1bbfb06f0d817e3005536ecef182
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 17:32:44 +0330

    Create header component for titles in main content pages

---
+++ src/components/MainContentHeader.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+const MainContentHeader = ({ title, desc }) => {
+  return (
+    <header>
+      <h1 className="text-5xl my-2 font-extrabold">{title}</h1>
+      <p className="text-lg">{desc}</p>
+    </header>
+  );
+};
+
+MainContentHeader.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+};
+
+export default MainContentHeader;
