0822e4ae6644fb69e49b89326f2c3d85580298eb
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 12:57:38 +0330

    Fix some bug and styles - Make category links absolute - Update categories to new filtered list - Add some styles to links

--- src/components/CategoriesSideNav.jsx
+++ src/components/CategoriesSideNav.jsx
@@ -4,11 +4,11 @@ import { toKebabCase } from '../services/utilities';
 const CategoriesSideNav = ({ categoryList, subCategories }) => {
   return (
     <nav className={`w-[300px]`}>
-      <ul className="divide-y-2  sticky top-0">
+      <ul className="divide-y-2  sticky top-4">
         {/* main nav link (default page) */}
         <li className={`text-xl px-6 hover:text-yellow-600  `}>
           <NavLink
-            to="new releases"
+            to="/games/new releases"
             className={`inline-block w-full py-4 transition-all font-bold`}
           >
             New Releases
@@ -16,20 +16,20 @@ const CategoriesSideNav = ({ categoryList, subCategories }) => {
         </li>
         {/* categories nav link */}
         {categoryList.map((category) => (
-          <li key={category.id}>
+          <li key={category}>
             <NavLink
-              to={category.id}
+              to={`/games/${category}`}
               className={({ isActive, isPending, isTransitioning }) =>
-                `text-lg w-full px-6 inline-block py-1 transition-[padding] text-gray-800/75 hover:bg-gradient-to-r from-transparent to-transparent via-15% ${isActive ? 'via-yellow-300/80 bg-gradient-to-r font-bold pl-10 peer is-active' : isPending || isTransitioning ? 'via-gray-400/30 bg-gradient-to-r font-bold' : 'via-gray-500/10'}`
+                `text-lg w-full px-6 inline-block py-2 transition-[padding] text-gray-800/75 hover:bg-gradient-to-r from-transparent to-transparent via-10% ${isActive ? 'via-yellow-200/90 bg-gradient-to-r font-bold peer py-4 is-active' : isPending || isTransitioning ? 'via-gray-400/30 bg-gradient-to-r font-bold' : 'via-gray-500/10'}`
               }
             >
-              {category.name}
+              {category}
             </NavLink>
             {subCategories && (
-              <nav className="flex-col hidden flex-nowrap peer-[.is-active]:flex pl-10">
+              <nav className="flex-col hidden flex-nowrap peer-[.is-active]:flex pl-6">
                 {subCategories.map((subCat) => (
                   <a
-                    className="py-1 px-4 text-sm rounded-sm font-bold opacity-50 hover:opacity-100 hover:border-l-4 border-yellow-300/80"
+                    className="py-2 px-4 text-sm text-gray-800/80 rounded-sm font-bold opacity-50 hover:opacity-100 hover:border-l-4 border-yellow-300/80"
                     key={subCat}
                     href={`#${toKebabCase(subCat)}`}
                   >
@@ -45,9 +45,7 @@ const CategoriesSideNav = ({ categoryList, subCategories }) => {
   );
 };
 CategoriesSideNav.propTypes = {
-  categoryList: PropTypes.arrayOf(
-    PropTypes.exact({ id: PropTypes.string.isRequired, name: PropTypes.string.isRequired })
-  ).isRequired,
+  categoryList: PropTypes.arrayOf(PropTypes.string).isRequired,
   subCategories: PropTypes.arrayOf(PropTypes.string),
 };
 export default CategoriesSideNav;
