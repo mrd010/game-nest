baf455365e0ca5ad67447ee75f52bd59d0e2330f
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 20:15:15 +0330

    Add video content to using previous list item

--- src/routes/Trailers.jsx
+++ src/routes/Trailers.jsx
@@ -1,16 +1,33 @@
 import { useLoaderData } from 'react-router-dom';
 import MainContentHeader from '../components/MainContentHeader';
 import MainContentContainer from '../components/MainContentContainer';
+import VideoListItem from '../components/VideoListItem';
+import { getCleanUrl } from '../services/utilities';
 
 const Trailers = () => {
   const trailers = useLoaderData();
   console.log(trailers);
+
+  const handleVideoOpen = () => {};
   return (
     <MainContentContainer>
       <MainContentHeader
         title={'Recent Videos'}
-        desc={'Watch the most recent videos from games for pc.'}
+        desc={'Watch the most recent videos of games for pc'}
       ></MainContentHeader>
+      <section className="grid grid-cols-4 gap-x-10 gap-y-16">
+        {trailers.map((trailerData) => (
+          <VideoListItem
+            key={`${trailerData.target.id}${trailerData.name}`}
+            gameName={trailerData.target.name}
+            name={trailerData.name}
+            thumbnail={getCleanUrl(trailerData.thumbnail)}
+            onSelect={handleVideoOpen}
+            iconCentered
+            className="grid grid-rows-[auto_auto] gap-2 content-start"
+          ></VideoListItem>
+        ))}
+      </section>
     </MainContentContainer>
   );
 };
