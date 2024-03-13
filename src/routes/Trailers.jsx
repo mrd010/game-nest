b9e7156d1676b8873315006e4a945d341fb59868
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 17:41:47 +0330

    Add base layout

--- src/routes/Trailers.jsx
+++ src/routes/Trailers.jsx
@@ -1,6 +1,18 @@
-import PropTypes from 'prop-types';
-const Trailers = (props) => {
-  return <div>Trailers</div>;
+import { useLoaderData } from 'react-router-dom';
+import MainContentHeader from '../components/MainContentHeader';
+import MainContentContainer from '../components/MainContentContainer';
+
+const Trailers = () => {
+  const trailers = useLoaderData();
+  console.log(trailers);
+  return (
+    <MainContentContainer>
+      <MainContentHeader
+        title={'Recent Videos'}
+        desc={'Watch the most recent videos from games for pc.'}
+      ></MainContentHeader>
+    </MainContentContainer>
+  );
 };
-Trailers.propTypes = {};
+
 export default Trailers;
