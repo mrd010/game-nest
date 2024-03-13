b5b943e4e4461eb9b3d82d54095c4c86c68bb499
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 21:19:17 +0330

    Make container relative

--- src/components/MainContentContainer.jsx
+++ src/components/MainContentContainer.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 const MainContentContainer = ({ children }) => {
-  return <div className="px-10 py-20 my-2 rounded-md bg-zinc-100 space-y-16">{children}</div>;
+  return (
+    <div className="px-10 relative py-20 my-2 rounded-md bg-zinc-100 space-y-16">{children}</div>
+  );
 };
 MainContentContainer.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
