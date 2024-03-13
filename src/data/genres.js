acc1a817aed13aa15781fac9459a7985279f36a7
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 12:58:03 +0330

    Add a genre filter list containing no use genres

---
+++ src/data/genres.js
@@ -0,0 +1,16 @@
+const genreFilterList = [
+  'genre_demos',
+  'Mac OS X',
+  'Linux',
+  'Controller support',
+  'Web Publishing',
+  'Video Production',
+  'Software Training',
+  'Photo Editing',
+  'Audio Production',
+  'Accounting',
+  'Utilities',
+  'Education',
+];
+
+export default genreFilterList;
