ef646d564686aa59c654c8571bca2759ed950311
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 21:20:09 +0330

    Add player in overlay when clicked video thumbnail

--- src/components/VideoPlayerOverlay.jsx
+++ src/components/VideoPlayerOverlay.jsx
@@ -1,17 +1,26 @@
-import PropTypes from 'prop-types';
+import PropTypes, { bool } from 'prop-types';
 import VideoPlayer from './VideoPlayer';
-const VideoPlayerOverlay = ({ hqUrl, lqUrl }) => {
+const VideoPlayerOverlay = ({ isOpen, hqUrl, lqUrl, onClose }) => {
   return (
-    <div>
-      <div>
+    <div
+      className={`fixed left-0 grid place-items-center top-0 bg-gray-900/90 z-50 w-full transition-transform duration-500 h-screen ${isOpen ? 'scale-100' : 'scale-0'}`}
+      style={{ margin: 0 }}
+    >
+      <button onClick={onClose} className="text-gray-50 grid scale-150 absolute top-5 right-5">
         <span className="material-symbols-rounded">close</span>
-      </div>
-      <VideoPlayer hqUrl={hqUrl} lqUrl={lqUrl}></VideoPlayer>
+      </button>
+      {isOpen && (
+        <div className="aspect-video w-[1000px]">
+          <VideoPlayer hqUrl={hqUrl} lqUrl={lqUrl}></VideoPlayer>
+        </div>
+      )}
     </div>
   );
 };
 VideoPlayerOverlay.propTypes = {
+  isOpen: bool.isRequired,
   hqUrl: PropTypes.string.isRequired,
   lqUrl: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 export default VideoPlayerOverlay;
