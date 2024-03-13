a0d65e970428b5e6f456b588e272df325f9dfd72
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-13 17:41:40 +0330

    Add check status before return

--- src/loaders/trailersLoader.js
+++ src/loaders/trailersLoader.js
@@ -2,5 +2,8 @@ import { getTrailers } from '../services/dataFetchers';
 
 export const trailersLoader = async () => {
   const trailers = await getTrailers();
-  return trailers;
+  if (trailers.status === 1) {
+    return trailers.movies;
+  }
+  return null;
 };
