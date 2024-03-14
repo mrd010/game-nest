a4953f2ee8598b600039ebc8ae061cbc04378fad
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-14 23:44:19 +0330

    Add game details page loader

--- src/loaders/gamePageLoader.js
+++ src/loaders/gamePageLoader.js
@@ -1,3 +1,9 @@
-export const gamePageLoader = ({ params }) => {
-  console.log(params);
+import { getGameDetails } from '../services/dataFetchers';
+
+export const gamePageLoader = async ({ params }) => {
+  if (!/^\d+$/.test(params.gameId)) {
+    return null;
+  }
+  const gameDetails = await getGameDetails(params.gameId);
+  return gameDetails;
 };
