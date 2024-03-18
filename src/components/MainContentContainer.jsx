c6ffed99011d818aeb1fc6652445ed3cc399871c
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-18 22:42:13 +0330

    Add custom styles to container

--- src/components/MainContentContainer.jsx
+++ src/components/MainContentContainer.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
-const MainContentContainer = ({ children }) => {
+const MainContentContainer = ({ className = '', children }) => {
   return (
-    <div className="px-10 relative py-20 my-2 rounded-md bg-zinc-100 space-y-16">{children}</div>
+    <div className={`px-10 relative py-20 my-2 rounded-md bg-zinc-100 space-y-16 ${className}`}>
+      {children}
+    </div>
   );
 };
 MainContentContainer.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
 };
 export default MainContentContainer;
