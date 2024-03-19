998d59ef2013abaa7b7a7302888e3e47481f8ea2
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-20 01:47:38 +0330

    Add simple carousel draggable

---
+++ src/components/SimpleCarousel.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { useRef } from 'react';
+import { useDraggable } from 'react-use-draggable-scroll';
+const SimpleCarousel = ({ children }) => {
+  const carousel = useRef(null);
+  const { events } = useDraggable(carousel);
+  console.log(events);
+
+  return (
+    <div
+      ref={carousel}
+      className={`grid carousel select-none py-2 items-start grid-flow-col auto-cols-max overflow-x-auto`}
+      {...events}
+    >
+      {children}
+    </div>
+  );
+};
+SimpleCarousel.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
+};
+export default SimpleCarousel;
