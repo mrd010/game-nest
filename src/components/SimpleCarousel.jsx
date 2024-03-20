5f1868510b674b27ebab9c7c22206b28672c1d16
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-20 15:34:51 +0330

    Add custom classes to component

--- src/components/SimpleCarousel.jsx
+++ src/components/SimpleCarousel.jsx
@@ -1,15 +1,14 @@
 import PropTypes from 'prop-types';
 import { useRef } from 'react';
 import { useDraggable } from 'react-use-draggable-scroll';
-const SimpleCarousel = ({ children }) => {
+const SimpleCarousel = ({ children, className = '' }) => {
   const carousel = useRef(null);
   const { events } = useDraggable(carousel);
-  console.log(events);
 
   return (
     <div
       ref={carousel}
-      className={`grid carousel select-none py-4 items-start grid-flow-col auto-cols-max overflow-x-auto`}
+      className={`grid carousel select-none items-start py-4 grid-flow-col auto-cols-max overflow-x-auto ${className}`}
       {...events}
     >
       {children}
@@ -18,5 +17,6 @@ const SimpleCarousel = ({ children }) => {
 };
 SimpleCarousel.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
+  className: PropTypes.string,
 };
 export default SimpleCarousel;
