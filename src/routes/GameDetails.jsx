9e66af8dc3747ecaf2be7c7998eedc35269fc7c3
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-21 01:18:06 +0330

    Make system requirement parts dense

--- src/routes/GameDetails.jsx
+++ src/routes/GameDetails.jsx
@@ -228,7 +228,7 @@ const GameDetails = () => {
       {/* system requirements */}
       <section>
         <HomeSectionTitle>PC Requirements</HomeSectionTitle>
-        <div className="grid grid-cols-3 grid-rows-2 gap-8 py-4">
+        <div className="grid grid-cols-3 grid-rows-2 grid-flow-col-dense gap-8 py-4">
           {Object.entries(systemRequirements).map((sysRequirement) => {
             const isLinuxOrMac = ['linux', 'mac'].includes(sysRequirement[0]);
             return (
