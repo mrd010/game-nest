315bc387ed134d73575757a3b60136809714cf29
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-22 01:46:18 +0330

    Add page loader

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,11 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation } from 'react-router-dom';
 import Header from './components/Header';
 
 const Layout = () => {
   const categories = useLoaderData();
+
+  // for navigation loader
+  const { state } = useNavigation();
 
   return (
     <div className="font-Lato min-h-screen bg-no-repeat bg-cover bg-center bg-fixed" id="main">
@@ -11,6 +14,11 @@ const Layout = () => {
       <main className="w-[1280px] mx-auto p-3">
         <Outlet context={categories}></Outlet>
       </main>
+      {state === 'loading' && (
+        <div className="fixed top-0 left-0 w-full h-screen bg-gray-50 grid place-items-center z-50">
+          <div className="page-loader "></div>
+        </div>
+      )}
     </div>
   );
 };
