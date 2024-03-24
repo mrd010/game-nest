e6002cc3a92ffce47c9065bbac9318155d4e62ac
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-25 00:42:15 +0330

    Add isMobile query checker

--- src/hooks/useDevices.js
+++ src/hooks/useDevices.js
@@ -2,6 +2,7 @@ import { useMediaQuery } from '@uidotdev/usehooks';
 
 export const useDevices = () => {
   const isHandheldDevice = useMediaQuery('only screen and (max-width:1023px)');
+  const isMobile = useMediaQuery('only screen and (max-width:767px)');
 
-  return { isHandheldDevice };
+  return { isHandheldDevice, isMobile };
 };
