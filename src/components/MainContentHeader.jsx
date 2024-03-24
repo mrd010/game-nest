b3df26e26aeacadb8c646d0141229d64efad0309
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-25 00:39:15 +0330

    Make font size of header smaller for large mobiles

--- src/components/MainContentHeader.jsx
+++ src/components/MainContentHeader.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 const MainContentHeader = ({ title, desc }) => {
   return (
     <header>
-      <h1 className="text-5xl my-2 font-extrabold">{title}</h1>
+      <h1 className="text-5xl md:text-3xl my-2 font-extrabold">{title}</h1>
       <p className="text-lg">{desc}</p>
     </header>
   );
