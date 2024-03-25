ab3310296a3fbc3e709d7ddd8536f8f1ea318726
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-26 01:41:18 +0330

    Decrease space between sections in mobile devices

--- src/components/MainContentContainer.jsx
+++ src/components/MainContentContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 const MainContentContainer = ({ className = '', children }) => {
   return (
     <div
-      className={`px-10 xl:px-6 relative py-20 lg:py-8 lg:bg-transparent my-2 rounded-md bg-zinc-100 space-y-16 lg:space-y-10 ${className}`}
+      className={`px-10 xl:px-6 relative py-20 lg:py-8 lg:bg-transparent my-2 rounded-md bg-zinc-100 space-y-16 lg:space-y-10 md:space-y-4 ${className}`}
     >
       {children}
     </div>
