61bd9dc6480c81a6ed54f4abec2477e407b48a98
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-25 22:19:45 +0330

    Add Swipe scrolling for mobile devices

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,6 +3,7 @@ import ActionButton from './ActionButton';
 import { useEffect, useRef, useState } from 'react';
 import HomeSectionTitle from './HomeSectionTitle';
 import { useOutletContext } from 'react-router-dom';
+import { useDrag } from '@use-gesture/react';
 
 const Carousel = ({
   title,
@@ -25,18 +26,41 @@ const Carousel = ({
   const nodeChildrenCount = children.filter((child) => child !== null).length;
 
   const handleScrollChange = (direction) => {
+    if (direction === 0) {
+      return;
+    }
     // when pressed arrow buttons change position between 0 and max items - steps -1
     // note: steps is number of items scrolling with each scroll
-    setCurrentPosition((p) => Math.min(Math.max(0, p + direction), nodeChildrenCount - steps - 1));
+    if (isMobile) {
+      setCurrentPosition((p) => Math.min(Math.max(0, p + direction), nodeChildrenCount - 1));
+    } else {
+      setCurrentPosition((p) =>
+        Math.min(Math.max(0, p + direction), nodeChildrenCount - steps - 1)
+      );
+    }
   };
 
   useEffect(() => {
     // after set of position scroll container to that position
     carousel.current.scrollLeft = currentPosition * itemWidth;
   }, [itemWidth, currentPosition]);
+
+  // gestures for mobile devices
+  useDrag(
+    ({ swipe: [sx] }) => {
+      console.log(sx);
+      handleScrollChange(-sx);
+    },
+    {
+      target: carousel,
+      enabled: isMobile,
+      axis: 'x',
+    }
+  );
+
   return (
     <div
-      className={`grid grid-cols-[1fr_auto] md:grid-cols-1 grid-row-2 relative gap-x-5 gap-y-5 md:gap-x-0 ${!disabledSidesBlur && currentPosition > 0 ? 'before:content-[" "] before:absolute before:bottom-2 before:left-0 before:z-50 before:h-[calc(100%_-_60px)] before:w-10 before:bg-gradient-to-l before:from-transparent before:to-gray-50 before:to-80%' : ''}${!disabledSidesBlur && currentPosition < nodeChildrenCount - steps - 1 ? 'after:content-[" "] after:w-8 after:right-0 after:bottom-2 after:h-[calc(100%_-_60px)] after:bg-gradient-to-r after:to-80% after:from-transparent after:to-gray-50 after:absolute after:z-50' : ''}`}
+      className={`grid grid-cols-[1fr_auto] md:grid-cols-1 relative gap-x-5 gap-y-5 md:gap-x-0 ${!disabledSidesBlur && currentPosition > 0 ? 'before:content-[" "] before:absolute before:bottom-2 before:left-0 before:z-50 before:h-[calc(100%_-_60px)] before:w-10 before:bg-gradient-to-l before:from-transparent before:to-gray-50 before:to-80%' : ''}${!disabledSidesBlur && currentPosition < nodeChildrenCount - steps - 1 ? 'after:content-[" "] after:w-8 after:right-0 after:bottom-2 after:h-[calc(100%_-_60px)] after:bg-gradient-to-r after:to-80% after:from-transparent after:to-gray-50 after:absolute after:z-50' : ''}`}
     >
       <HomeSectionTitle>{title}</HomeSectionTitle>
       {!isMobile && (
@@ -58,12 +82,22 @@ const Carousel = ({
         </div>
       )}
       {children && (
-        <div
-          className="grid grid-flow-col auto-cols-max scroll-smooth col-span-2 overflow-hidden pb-4 sm:pb-0 carousel"
-          ref={carousel}
-        >
-          {children}
-        </div>
+        <>
+          <div
+            className="grid grid-flow-col auto-cols-max scroll-smooth col-span-2 overflow-hidden pb-4 md:pb-0 carousel touch-none"
+            ref={carousel}
+          >
+            {children}
+          </div>
+          <div className="flex flex-row flex-nowrap gap-2 justify-center">
+            {[...Array(nodeChildrenCount)].map((v, index) => (
+              <div
+                key={index}
+                className={`size-2  rounded-full bg-gray-900 transition-opacity ${currentPosition === index ? 'opacity-100' : ' opacity-25'}`}
+              ></div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
