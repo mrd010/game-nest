a1371061c25fcdfae01f239efa25296da8db0f02
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-25 12:59:44 +0330

    Change size of card for mobile devices

--- src/components/RecommendedCard.jsx
+++ src/components/RecommendedCard.jsx
@@ -25,13 +25,13 @@ const RecommendedCard = ({
       className="grid grid-rows-[minmax(0,1fr)_auto_auto] p-3 md:p-2 h-full drop-shadow-sm shadow-md bg-gradient-to-b from-amber-300 from-25% md:from-20% to-25% md:to-20% to-gray-100 rounded-md"
     >
       {/* header */}
-      <div className="grid grid-rows-[124px_minmax(0,1fr)_auto] md:grid-rows-[100px_minmax(0,1fr)_auto] gap-3 pb-2">
+      <div className="grid grid-rows-[124px_minmax(0,1fr)_auto] md:grid-rows-[110px_minmax(0,1fr)_auto] gap-3 pb-2">
         <div className="size-full relative rounded-md bg-gray-100">
           {!imageLoaded && <div className="image-loader rounded-md"></div>}
           <LazyLoadImage
             src={steamHeaderImage(steamAppID)}
             alt={`${title} image`}
-            className="h-[124px] md:h-[100px] w-full rounded-md"
+            className="h-[124px] md:h-[110px] w-full rounded-md"
             onLoad={() => setImageLoaded(true)}
           ></LazyLoadImage>
         </div>
@@ -45,11 +45,11 @@ const RecommendedCard = ({
       </div>
       {/* other details */}
       <GameScore>
-        <GameScore.GameScoreTitle>{isMobile ? 'Meta' : 'Metascore'}</GameScore.GameScoreTitle>
+        <GameScore.GameScoreTitle>Metascore</GameScore.GameScoreTitle>
         <GameScore.GameScoreRateMeta score={Number(metacriticScore)}></GameScore.GameScoreRateMeta>
       </GameScore>
       <GameScore>
-        <GameScore.GameScoreTitle>Steam {isMobile ? '' : 'Score'}</GameScore.GameScoreTitle>
+        <GameScore.GameScoreTitle>Steam Score</GameScore.GameScoreTitle>
         <GameScore.GameScoreRateSteam
           score={Number(steamRatingPercent)}
         ></GameScore.GameScoreRateSteam>
