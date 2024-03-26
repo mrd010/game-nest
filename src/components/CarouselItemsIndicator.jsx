e405d2a042d3b7b53435b05a9a95a2d0a7abe5bd
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-27 00:10:14 +0330

    Make current indicator more fancy

--- src/components/CarouselItemsIndicator.jsx
+++ src/components/CarouselItemsIndicator.jsx
@@ -3,10 +3,11 @@ const CarouselItemsIndicator = ({ count, current }) => {
   return (
     <div className="flex flex-row flex-nowrap gap-2 justify-center">
       {[...Array(count)].map((v, index) => (
-        <div
-          key={index}
-          className={`size-2 rounded-full bg-gray-900 transition-opacity ${current === index ? 'opacity-100' : ' opacity-25'}`}
-        ></div>
+        <div key={index} className={`size-2 rounded-full bg-gray-900/15`}>
+          <div
+            className={`size-full rounded-full z-10 transition-transform bg-gray-900/80 duration-300 ${current === index ? 'scale-125' : 'scale-0'}`}
+          ></div>
+        </div>
       ))}
     </div>
   );
