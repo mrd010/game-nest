c8fc591ca7df6707734574a1c33fa044b9b88439
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-27 00:26:44 +0330

    Add error handler for invalid data

--- src/loaders/gamePageLoader.js
+++ src/loaders/gamePageLoader.js
@@ -5,5 +5,8 @@ export const gamePageLoader = async ({ params }) => {
     return null;
   }
   const gameDetails = await getGameDetails(params.gameId);
+  if (!gameDetails) {
+    throw new Error('Not Found');
+  }
   return gameDetails;
 };
