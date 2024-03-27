54cde7004b94f63321eb8a98ceae56bb01d205d2
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-28 01:40:45 +0330

    Make search button disabled opacity lower

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -77,8 +77,8 @@ const HeaderSearch = () => {
         {/* search button */}
         <button
           type="submit"
-          disabled={query.length <= 3}
-          className="absolute right-0 top-0 opacity-75 p-2 text-2xl scale-125 hover:opacity-100 transition-opacity"
+          disabled={query.length < 3}
+          className="absolute right-0 top-0 opacity-75 p-2 text-2xl scale-125 hover:opacity-100 transition-opacity disabled:opacity-20"
         >
           <span className="material-symbols-rounded">search</span>
         </button>
