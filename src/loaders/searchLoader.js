08fa9025fda41c4ffd2f39b1eb51cb32c9d1b663
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-03-28 01:41:10 +0330

    Add error when query is 3 length lower

--- src/loaders/searchLoader.js
+++ src/loaders/searchLoader.js
@@ -4,7 +4,10 @@ export const searchLoader = async ({ request }) => {
   // get query from search parameters
   const url = new URL(request.url);
   const query = url.searchParams.get('q');
-  //   search for query and return data
+  //   search for query and return data if query is 3> length
+  if (query.length < 3) {
+    throw new Error('No Data for sacredly cats');
+  }
   const searchResults = await getSearchResults(query, 0);
   return { searchResults, query };
 };
